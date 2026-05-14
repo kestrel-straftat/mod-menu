4ae0f08aafd56f51412517760c0c017d5b92db55
sneakykestrel <141267380+sneakykestrel@users.noreply.github.com>
2026-05-14 04:18:26 +0100

    Fix numeric sliders throwing if option's AcceptableValueRange does not include 0

--- Assets/Scripts/Behaviours/OptionList/ValueControllers/NumericSliderValueController.cs
+++ Assets/Scripts/Behaviours/OptionList/ValueControllers/NumericSliderValueController.cs
@@ -11,6 +11,7 @@ namespace ModMenu.Behaviours.OptionList.ValueControllers
         public Slider slider;
         public TMP_InputField inputField;
         
+        private bool m_boundsSet;
         private bool m_isIntegralType;
 
         protected override void Setup() {
@@ -20,12 +21,20 @@ namespace ModMenu.Behaviours.OptionList.ValueControllers
         internal override void SetupFromOption(Option option) {
             var range = option.AcceptableValues;
             var valueRangeType = range.GetType();
+            // note: These will result in a call to OnSliderValueChanged if 0 is outside of the new range!
+            // (unity does not provide a way to set slider bounds without notify. bastards)
             slider.minValue = Convert.ToSingle(valueRangeType.GetProperty("MinValue")!.GetValue(range));
             slider.maxValue = Convert.ToSingle(valueRangeType.GetProperty("MaxValue")!.GetValue(range));
             base.SetupFromOption(option);
+            m_boundsSet = true;
         }
 
         public void OnSliderValueChanged(float value) {
+            // if this is false we're (probably) recieving an event from ourselves setting up the slider
+            if (!m_boundsSet) {
+                return;
+            }
+            
             if (Convert.ChangeType(value, ValueType) is { } converted) {
                 setter.Invoke(converted);
             }
